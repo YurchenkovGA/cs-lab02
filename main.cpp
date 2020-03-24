931f7ccbfcd2e1060148231c6b6c350fe4530648
Bob (YurchenkovGA) <Bob@yandex.ru>
2020-03-24 20:47:37 +0300

    code: minimum

--- main.cpp
+++ main.cpp
@@ -12,8 +12,15 @@ int main()
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     return 0;
+
     if (a>b)
 	cout << " maximum = " << a;
     else
 	cout << " maximum = " << b;
+
+    if (a<b)
+	cout << " minimum = " << a;
+    else
+	cout << " minimum = " << b;
+
 }
